001f763068f6c5bc4b3b82f748ba9e854ab41015
Awopeju Kolawole Moses <Awopejumoses@gmail.com>
2024-11-05 06:08:47 +0100

    Step 1

--- gsap/animations/src/pages/GsapTo.jsx
+++ gsap/animations/src/pages/GsapTo.jsx
@@ -1,9 +1,19 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
-import React from 'react'
+import React, { useEffect } from 'react'
+
 
 const GsapTo = () => {
+
+    useGSAP(() => {
+        gsap.to('#blue-box', {
+            x: 250,
+            repeat: -1,
+            yoyo: true,
+        })
+    }, [])
+    
   return (
     <main>
         <h1 className="text-center mt-10">GSAP Animation Demo</h1>
